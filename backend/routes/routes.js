6af2ff665e84bc7ca20a562034df12f8f7e75c0f
Mario Markov <mario11septemvri@gmail.com>
2023-02-09 13:53:56 +0200

    Fixed path for storage

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -9,7 +9,7 @@ const {resolve} = require('path');
 //Configure multer
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, './public')
+      cb(null, './public/images')
     },
   
     filename: (req, file, cb)=>{
