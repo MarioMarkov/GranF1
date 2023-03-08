f6fd8fe2da1ea46492350108e99a466904dc2159
MarioMarkov <mario11septemvri@gmail.com>
2023-03-08 10:37:07 +0200

    Added conditional login options in ui

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect} from 'react';
 import './Navbar.css';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router";
@@ -29,10 +29,19 @@ function Navbar() {
                 <Link to="/articles">Race Reviews</Link> 
                 <Link to="/articles">F1 Stories</Link> 
                 <Link to="/about">About</Link> 
-                <Link to="/add">Add Article</Link>
-                <Button variant="primary" onClick={handleLogout}>
+          <Link to="/add">Add Article</Link>
+          { user && <Button variant="primary" onClick={handleLogout}>
                     Log out
-                </Button>
+          </Button>}
+          {!user &&
+            <>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/login">Log In</Link>
+
+            </>
+          }
+                
+          <span>User : { user && user.email}</span>
         </div>
             
           
