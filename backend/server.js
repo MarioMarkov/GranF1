a7c34a8dbe83c789bd73438f589d49c6e15e6b03
Mario Markov <mario11septemvri@gmail.com>
2023-03-11 18:18:39 +0200

    Allowed CORS

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
