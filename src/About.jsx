ce3e14913a2ca5177bea616457b87bc0a35c97da
MarioMarkov <mario11septemvri@gmail.com>
2023-06-27 15:01:59 +0200

    Changed about

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import { React } from "react";
 function About({ setLang, t, i18n }) {
   return (
     <div className="w-[90%] md:w-[50%] m-auto text-lg border-4 border-purple p-4">
-      {t("about_text")} <br />
+      {/* {t("about_text")} <br /> */}
       <br />
       {t("about_text2")} <br />
       React Js as a Front End ⚛️ <br />
